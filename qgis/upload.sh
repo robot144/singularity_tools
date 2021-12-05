ae5e43dc9c4148c15bcbfde0915104d433951162
Robot144 <robot144@protonmail.com>
2021-12-05 22:13:19 +0100

    Add upload scripts

--- qgis/upload.sh
+++ qgis/upload.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # needs key-pair (generate first)
-singularity sign build/cdo.sif
+singularity sign build/qgis.sif
 
 # needs key at singularity index https://cloud.sylabs.io/library
-singularity push build/cdo.sif library://robot144/default/cdo:latest
+singularity push build/qgis.sif library://robot144/default/qgis:latest
